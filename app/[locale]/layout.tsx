4b6f2f329affcbb22b78f0032a70566d063d5d28
Federico Avelin <fede.avelin.dev@gmail.com>
2025-04-01 20:56:49 -0300

    feat: add setup for next-intl

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,45 @@
+import { routing } from "@/i18n/routing";
+import type { Metadata } from "next";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages, setRequestLocale } from "next-intl/server";
+import { notFound } from "next/navigation";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Federico Avelin | FullStack Web Developer",
+  description: "Portfolio of Federico Avelin, a FullStack Web Developer",
+};
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
+
+type Props = Readonly<{
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}>;
+
+export default async function RootLayout({ children, params }: Props) {
+  const { locale } = await params;
+
+  // Ensure that the incoming `locale` is valid
+  if (!routing.locales.includes(locale)) {
+    notFound();
+  }
+
+  setRequestLocale(locale);
+
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
---
+++ i18n/routing.ts
@@ -0,0 +1,23 @@
+import { createNavigation } from "next-intl/navigation";
+import { defineRouting } from "next-intl/routing";
+
+export const DEFAULT_LOCALES = ["en", "es"];
+
+export const routing = defineRouting({
+  locales: DEFAULT_LOCALES,
+  defaultLocale: "en",
+  pathnames: {},
+  localePrefix: "as-needed",
+});
+
+export type Pathnames = keyof typeof routing.pathnames;
+export type Locale = (typeof routing.locales)[number];
+
+export const {
+  Link,
+  permanentRedirect,
+  redirect,
+  usePathname,
+  useRouter,
+  getPathname,
+} = createNavigation(routing);
