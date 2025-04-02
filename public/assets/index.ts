314ab3697f378f1a991207e27813a6c92142265d
Federico Avelin <fede.avelin.dev@gmail.com>
2025-04-01 22:42:57 -0300

    refactor: move some images to public folder

---
+++ public/assets/index.ts
@@ -0,0 +1,6 @@
+import argFlag from "./argFlag.png";
+import hamburger from "./hamburger.png";
+import logo from "./logo.png";
+import usaFlag from "./usaFlag.png";
+
+export { argFlag, hamburger, logo, usaFlag };
