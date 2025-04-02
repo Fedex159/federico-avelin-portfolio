0ae197dc990be929f405d4931487e8922727722c
Federico Avelin <fede.avelin.dev@gmail.com>
2025-04-01 22:53:55 -0300

    refactor: move profile image to public assets

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,6 +1,7 @@
 import argFlag from "./argFlag.png";
 import hamburger from "./hamburger.png";
 import logo from "./logo.png";
+import profileImage from "./profile.jpg";
 import usaFlag from "./usaFlag.png";
 
-export { argFlag, hamburger, logo, usaFlag };
+export { argFlag, hamburger, logo, profileImage, usaFlag };
